6ffd4552dc07d2a13c342618b64306aa67823b62
gayathrimh <121299528+gayathrimh@users.noreply.github.com>
2025-11-26 16:19:21 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+n1=5
+n2=8
+sum1=n1+n2
+diff1=n1-n2
+prd1=n1*n2
+div1=n1/n2
+div2=n1//n2
+rem1=n1%n2
+print(sum1,diff1,prd1,div1,div2,rem1)
+
---
+++ 2.py
@@ -0,0 +1,12 @@
+def calc(n1,n2):
+    sum1=n1+n2
+    diff1=n1-n2
+    prd1=n1*n2
+    div1=n1/n2
+    div2=n1//n2
+    rem1=n1%n2
+    print(sum1,diff1,prd1,div1,div2,rem1)
+calc(10,2)
+calc(2,10)
+calc(8,4)
+
