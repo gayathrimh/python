6ee5994eb4807d549153aea276a9f3372cd1c20d
Gayathri <gayathrimh11@gmail.com>
2026-01-07 12:42:00 +0530

    Add 14.py

--- 14.py
+++ 14.py
@@ -1,58 +1,3 @@
-<<<<<<< HEAD
-#2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,61,67,71,73,77,79,81,83,87,89,91,97
-
-list1=[2,3]
-print(list1)
-num1=4
-isPrime=True
-if num1%2==0:
-  isPrime=False
-if num1%3==0:
-  isPrime=False
-if isPrime==True:
-    list1.append(num1)
-print(list1)
-
-num1=5
-isPrime=True
-if num1%2==0:
-    isPrime=False
-if num1%3==0:
-    isPrime==False
-if isPrime==True:
-    list1.append(num1)
-    print(list1)
-
-num1=6
-isPrime=True
-if num1%2==0:
-    isPrime=False
-if num1%3==0:
-    isPrime=False
-if num1%4==0:
-    isPrime=False
-if num1%5==0:
-    isPrime=False
-if isPrime==True:
-    list1.append(num1)
-    print(list1)
-
-num1=7
-isPrime=True
-if num1%2==0:
-    isPrime=False
-if num1%3==0:
-    isPrime=False
-if num1%4==0:
-    isPrime=False
-if num1%5==0:
-    isPrime=False
-if num1%6==0:
-    isPrime=False
-if isPrime==True:
-    list1.append(num1)
-    print(list1)
-=======
 f1=open("in2.txt","r")
 f2=open("out1.txt","w")
 s1=f1.readline().strip()
@@ -68,4 +13,3 @@ for j in range(n1,n2+1,1):
     print()
     f2.write("\n")
 f2.close()
->>>>>>> 2662dd6074379fe70661297c0b2e2e35399080c9
