b226619ebb553e36340f99c0e817c58989e5c7e8
gayathrimh11@gmail.com <gayathrimh11@gmail.com>
2026-02-18 10:56:48 +0530

    Multiplication using input txt file in list form

--- 12.py
+++ 12.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 f1=open("in2.txt")
 s1=f1.readline().strip()
 list1=s1.split(",")
@@ -9,15 +8,3 @@ for j in range(n1,n2+1,1):
     for i in range(1,11,1):
         print(j,i,j*i)
     print()    
-=======
-f1=open("in2.txt")
-s1=f1.readline().strip()
-list1=s1.split(",")
-n1=int(list1[0])
-n2=int(list1[1])
-for j in range(n1,n2+1,1):
-    
-    for i in range(1,11,1):
-        print(j,i,j*i)
-    print()    
->>>>>>> 2662dd6074379fe70661297c0b2e2e35399080c9
