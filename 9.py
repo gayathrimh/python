c7835b6c131c37af16780c90302994bf0f52dd67
gayathrimh11@gmail.com <gayathrimh11@gmail.com>
2026-02-18 10:43:03 +0530

    Multiplication table within the range using input functions

--- 9.py
+++ 9.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 n1=int(input("enter first number"))
 n2=int(input("enter second number"))
 for j in range(n1,n2+1,1):
@@ -6,12 +5,3 @@ for j in range(n1,n2+1,1):
     for i in range(1,11,1):
         print(j,i,j*i)
     print()    
-=======
-n1=int(input("enter first number"))
-n2=int(input("enter second number"))
-for j in range(n1,n2+1,1):
-    
-    for i in range(1,11,1):
-        print(j,i,j*i)
-    print()    
->>>>>>> 2662dd6074379fe70661297c0b2e2e35399080c9
