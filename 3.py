0e130e805beda315209a854d66ad79936b92eeaa
gayathrimh11@gmail.com <gayathrimh11@gmail.com>
2026-02-18 10:28:08 +0530

    Multiplication table of 3 upto 5 using for loop

--- 3.py
+++ 3.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 for i in range(1,11,1):
     print(3,i,3*i)
 print()    
@@ -8,14 +7,4 @@ print()
 for i in range(1,11,1):
     print(5,i,5*i)
 print()
-=======
-for i in range(1,11,1):
-    print(3,i,3*i)
-print()    
-for i in range(1,11,1):
-    print(4,i,4*i)
-print()
-for i in range(1,11,1):
-    print(5,i,5*i)
-print()
->>>>>>> 2662dd6074379fe70661297c0b2e2e35399080c9
+
