828a1b47eb1dd426f2512b1e0e174454ca299a7a
gayathrimh11@gmail.com <gayathrimh11@gmail.com>
2026-02-18 11:38:22 +0530

    Adding calculator using function calc

---
+++ 46.py
@@ -0,0 +1,14 @@
+def calc1(num1,num2):
+    sum1=num1+num2
+    dif1=num1-num2
+    prd1=num1*num2
+    div1=num1/num2
+    div2=num1//num2
+    rem1=num1%num2
+    exp1=num1**num2
+    print(sum1,dif1,prd1,div1,div2,rem1,exp1)
+calc1(2,5)
+calc1(5,2)
+
+
+
