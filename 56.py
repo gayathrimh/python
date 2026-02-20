2f2b2f5f42c17ae6cfe4b01c4e4dd1e27b95e8cb
gayathrimh11@gmail.com <gayathrimh11@gmail.com>
2026-02-20 15:04:04 +0530

    Implemented file handling to read range from in2.txt and write multiplication tables to out1.txt

---
+++ 56.py
@@ -0,0 +1,16 @@
+f1=open("in2.txt","r")
+s1=f1.readline().strip()
+list1=s1.split(",")
+n1=int(list1[0])
+n2=int(list1[1])
+f1.close()
+
+f2=open("out1.txt","w")
+for j in range(n1,n2+1,1):
+    for i in range(1,11,1):
+        print(j,i,j*i)
+        f2.write(str(j)+"*"+str(i)+"="+str(j*i))
+        f2.write("\n")
+    print()
+    f2.write("\n")
+f2.close()
