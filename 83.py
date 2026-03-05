c14b7d3f02874697962834bba8ed177dd63c7b2e
gayathrimh11@gmail.com <gayathrimh11@gmail.com>
2026-03-05 17:52:16 +0530

    Square, cube, factorial, factors function added to mod_toolbox.py, test using 83.py

---
+++ 83.py
@@ -0,0 +1,27 @@
+import mod_toolbox as mt
+
+n=4
+list1=[5,6,7]
+
+result=mt.square1(n)
+print("square of",n ,"is",result)
+
+result=mt.square2(list1)
+print("square of",list1,"are",result)
+
+result=mt.cube1(n)
+print("cube of",n ,"is",result)
+
+result=mt.cube2(list1)
+print("cube of",list1,"are",result)
+
+result=mt.factorial1(n)
+print("factorial of",n ,"is",result)
+
+result=mt.factorial2(list1)
+print("factorial of",list1,"are",result)
+
+n=42
+result=mt.factors(n)
+print("factors of",n,"is",result)
+
