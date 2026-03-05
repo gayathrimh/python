35238073f66cc38e0a316ba13c5521a8a014a37f
gayathrimh11@gmail.com <gayathrimh11@gmail.com>
2026-03-05 16:51:06 +0530

    Finding prime numbers from 2 to 100

---
+++ 74.py
@@ -0,0 +1,40 @@
+#2,3,5,7,11,13,17,19,23,29,31,41,43,47,53,59,61,67,71,73,79,83,89,97
+
+list1=[2,3]
+print(list1)
+num1=4
+isPrime=True
+if num1%2==0:
+    isPrime=False
+if num1%3==0:
+    isPrime=False
+if isPrime==True:
+    list1.append(num1)
+print(list1)
+
+num1=5
+isPrime=True
+if num1%2==0:
+    isPrime=False
+if num1%3==0:
+    isPrime=False
+if num1%4==0:
+    isPrime=False
+if isPrime==True:
+    list1.append(num1)
+print(list1)
+
+num1=6
+isPrime=True
+if num1%2==0:
+    isPrime=False
+if num1%3==0:
+    isPrime=False
+if num1%4==0:
+    isPrime=False
+if num1%5==0:
+    isPrime=False
+if isPrime==True:
+    list1.append(num1)
+print(list1)
+    
---
+++ 76.py
@@ -0,0 +1,13 @@
+#2,3,5,7,11,13,17,19,23,29,31,41,43,47,53,59,61,67,71,73,79,83,89,97
+
+list1=[2,3]
+for j in range(4,7,1):
+    num1=j
+    isPrime=True
+    for i in range(2,j,1):
+        if num1%i==0:
+            isPrime=False
+    if isPrime==True:
+        list1.append(num1)
+print(list1)
+
