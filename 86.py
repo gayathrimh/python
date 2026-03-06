624bd1fa42a33b944ebe6ed991c14be8bde72e3f
gayathrimh11@gmail.com <gayathrimh11@gmail.com>
2026-03-06 11:03:06 +0530

    Rversing number using string str

---
+++ 86.py
@@ -0,0 +1,26 @@
+#Reverse a number
+
+n=1234
+list1=[]
+temp1=n%10   #4
+temp2=n//10  #123
+list1.append(str(temp1))
+print(list1)
+
+n=temp2
+temp1=n%10  #3
+temp2=n//10 #12
+list1.append(str(temp1))
+print(list1)
+
+n=temp2
+temp1=n%10  #2
+temp2=n//10 #1
+list1.append(str(temp1))
+print(list1)
+
+n=temp2
+temp1=n%10  #1
+temp2=n//10
+list1.append(str(temp1))
+print(list1)
