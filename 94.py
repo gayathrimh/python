8aeffb7b04f8eaf4d7043aead6070fbca37404f4
gayathrimh11@gmail.com <gayathrimh11@gmail.com>
2026-03-09 15:26:34 +0530

    Simple calculator program

---
+++ 94.py
@@ -0,0 +1,12 @@
+def calc1(n1,n2):
+    sum1=n1+n2
+    dif1=n1-n2
+    mul1=n1*n2
+    div1=n1/n2
+    div2=n1//n2
+    rem1=n1%n2
+    exp1=n1**n2
+    print(sum1,dif1,mul1,div1,div2,rem1,exp1)
+calc1(2,6)
+calc1(4,8)
+calc1(-7,2)
