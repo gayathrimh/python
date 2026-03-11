40d80aff56520ebb4a0ba08ede0279d7a713f15c
gayathrimh11@gmail.com <gayathrimh11@gmail.com>
2026-03-11 15:12:09 +0530

    Full diamond

---
+++ 104.py
@@ -0,0 +1,7 @@
+s1="FunwithProgrammings"
+len1=len(s1)
+len2=len1//2
+for i in range(0,len2+1,1):
+    print(" "*(len2-i)+s1[0:1+2*i])
+for i in range(1,len2+1,1):
+    print(" "*i+s1[0:(len1-i)-i])
